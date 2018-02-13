0e828291ccbc9a9032c6458040a21d8628927b1c
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-02-13 10:37:40 -0700

    finished giphy app, added how many field, added empty function

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
   $("#subButton").click(function() {
+    $(".gifList").empty();
     var query = $("#inputText").val();
     var before = "https://api.giphy.com/v1/gifs/search?api_key=kGuuTSUioSRP8OmGMJY6XMBslT0VuxHZ&q=";
-    var after = "&limit=25&offset=0&rating=G&lang=en"
+    var after = "&limit=" + $("#inputNum").val(); + "&offset=0&rating=G&lang=en"
     var request = before + query + after;
     $.ajax(
       {
+        method: "GET",
         url: request,
-        success: function(result)
-      {
-      console.log(result.data[1].images.downsized_medium.url);
+        success: function(result){
+          for (i = 0; i < result.data.length; i++){
+          $(".gifList").append("<img src=" + result.data[i].images.original.url + "></img>");
+        }
     }});
   });
 });
-
-
-
-
-//   $.ajax({method: "GET", url: "https://api.giphy.com/v1/gifs/search?api_key=kGuuTSUioSRP8OmGMJY6XMBslT0VuxHZ&q=&limit=25&offset=0&rating=G&lang=en"}.done(response))
-// });
